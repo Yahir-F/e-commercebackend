9bcd647064aaea6a38706f7018b4c04a72ccf6d7
Yahir Federico <107011404+Yahir-F@users.noreply.github.com>
2022-08-01 21:58:31 -0700

    finished routes

--- models/index.js
+++ models/index.js
@@ -8,23 +8,23 @@ const ProductTag = require('./ProductTag');
 
 
 
-// Products belongsTo Category
+
 
 Product.belongsTo(Category, {
-  foreignKey: 'category_id',
+  foreignKey: 'cat_id',
 });
-// Categories have many Products
+
 
 Category.hasMany(Product, {
-  foreignKey: 'category_id',
+  foreignKey: 'cat_id',
 });
-// Products belongToMany Tags (through ProductTag)
+
  
 Product.belongsToMany(Tag, {
   through: ProductTag,
   foreignKey: 'product_id',
 });
-// Tags belongToMany Products (through ProductTag)
+
 Tag.belongsToMany(Product, {
   through: ProductTag,
   foreignKey: 'tag_id',
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
+
 
 router.get('/', (req, res) => {
   Category.findAll({
@@ -12,7 +12,7 @@ router.get('/', (req, res) => {
       }
     ]
   })
-    .then((catData) => res.json(catData))
+    .then((caterData) => res.json(caterData))
     .catch((err) => {
       res.status(500).json(err);
   });
@@ -20,20 +20,73 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'catergory_id'],
+      }
+    ]
+  })
+    .then((caterData) => {
+      if (!caterData) {
+        res.status(404).json({ message: 'Invalid Category or Does not exist' });
+        return;
+      }
+      res.json(caterData);
+    }).catch((err) => {
+      res.status(500).json(err);
+    }
+  );
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+    .then((caterData) => res.json(caterData))
+        .catch((err) => {
+         res.status(500).json(err);
+    }
+  );
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  Category.update(
+    req.body, {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+    .then((caterData) => {
+      if (!caterData) {
+        res.status(500).json({ message: 'Invalid Category or Does not exist' });
+        return;
+      }
+      res.json(caterData);
 });
-
+});
 router.delete('/:id', (req, res) => {
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then((caterData) => {
+      if (!caterData) {
+        res.status(500).json({ message: 'Invalid Category or Does not exist' });
+        return;
+      }
+      res.json(caterData);
+    }).catch((err) => {
+      res.status(500).json(err);
+    });
+  });
+
   // delete a category by its `id` value
-});
 
 module.exports = router;
