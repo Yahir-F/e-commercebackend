bff0eb21db94eea796719c30216c90d0a7f114e8
Lucid <107011404+Yahir-F@users.noreply.github.com>
2022-08-01 19:07:39 -0700

    added ids for productTag

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,8 +6,29 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
   },
+  product_id: { 
+    type: DataTypes.INTEGER,
+    allowNull: false,
+        references: {
+            model: 'product',
+            key: 'id',
+        }
+  },
+     tag_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'tag',
+        key: 'id',
+         },
+     }
+ },
   {
     sequelize,
     timestamps: false,
