c1071cef264b9a877a2ec43d3413ac616878a55a
Yahir Federico <107011404+Yahir-F@users.noreply.github.com>
2022-08-01 20:21:43 -0700

    created routes for categories and tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,8 +4,19 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll({
+    attributes: ['id', 'tag_name'],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'category_id'],
+      }
+    ]
+  })
+    .then((DataTag) => res.json(DataTag))
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
