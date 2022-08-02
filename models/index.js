143fc279f17d1658ffe60db7eb57712b11d28c9a
Lucid <107011404+Yahir-F@users.noreply.github.com>
2022-08-01 19:28:46 -0700

    finished setting up index model

--- models/index.js
+++ models/index.js
@@ -4,15 +4,32 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+
+
+
+
 // Products belongsTo Category
+
 Product.belongsTo(Category, {
   foreignKey: 'cat_id',
 });
 // Categories have many Products
 
+Category.hasMany(Product, {
+  foreignKey: 'cat_id',
+});
 // Products belongToMany Tags (through ProductTag)
-
+ 
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
+
 
 module.exports = {
   Product,
